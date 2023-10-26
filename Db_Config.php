bec095be3f72d371718314152f31186fab752ee1
ricardojnva <143089452+ricardojnva@users.noreply.github.com>
2023-10-26 00:36:54 -0700

    Update Db_Config.php

--- Db_Config.php
+++ Db_Config.php
@@ -4,8 +4,8 @@ $db_user = "root";
 $db_password = "";
 $db_name = "Db_LabLogging";
 
-$db_connect = mysqli_connect($db_host, $db_user, $db_password, $db_name);
+$conn = mysqli_connect($db_host, $db_user, $db_password, $db_name);
 
-if (!$db_connect) {
+if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
